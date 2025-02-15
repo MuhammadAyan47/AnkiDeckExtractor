19bdbce868a511e276928e0174af581ef8512aed
Aidan Juma <aidan@aidanjuma.dev>
2025-02-15 01:21:19 +0000

    ✨ basic CLI application working

---
+++ main.py
@@ -0,0 +1,40 @@
+import argparse
+import sys
+from pathlib import Path
+
+
+def main():
+    # Define parser & relevant arguments to enable basic CLI functionality:
+    parser = argparse.ArgumentParser(
+        description='Unpack .apkg archives (Anki Flashcard Deck) into their derivative directories & files/metadata.')
+    parser.add_argument('apkg', type=str, help='Path to the apkg archive.')
+    parser.add_argument('output', type=str, help='Path to the output directory.', default='./output')
+
+    # Parse arguments for processing:
+    args = parser.parse_args()
+
+    # Convert input for paths into more workable format (Path from pathlib):
+    apkg_path = Path(args.apkg)
+    output_path = Path(args.output)
+
+    # Cannot continue if APKG doesn't exist at provided path/is not a file; must also be of type .apkg.
+    if not apkg_path.exists():
+        sys.exit(f'❌ >> Error: {apkg_path} does not exist.')
+    if not apkg_path.is_file():
+        sys.exit(f'❌ >> Error: {apkg_path} is not a file.')
+    if not apkg_path.suffix.lower() == '.apkg':
+        sys.exit(f'❌ >> Error: {apkg_path} is not an .apkg file.')
+
+    # Attempt to create the output directory:
+    try:
+        output_path.mkdir(parents=True, exist_ok=True)
+    except Exception as e:
+        sys.exit(
+            f'❌ >> Error: Cannot create or access output directory at {output_path}.\n📝 The following exception was raised:\n{e}')
+
+    # TODO: Write logic within src/ in packages, and then use them here to perform extraction/processing.
+    exit(0)
+
+
+if __name__ == '__main__':
+    main()
